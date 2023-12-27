c85ae22a0433ab6c69ff04585cd8a7336d55b26e
David Hitchen <hitchen28963@yahoo.com>
2023-12-27 16:03:40 +0000

    added even listener and functions for later use

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+// Wait for the DOM to finish loading before running the game
+// Get for button elements and add event listeners to them
+
+document.getEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons){
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
+            } else {
+                let gameType = this.getAttribute("data-type")
+                alert(`you clicked ${gameType}`);
+            }
+        })
+    }
+})
+
 function runGame() {
 
 }
